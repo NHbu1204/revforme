3d3534ec3271925b7c75702fe052347a695e7dcc
NHbu1204 <anonyapollo@gmail.com>
2025-05-09 16:10:07 +0400

    Add files via upload

---
+++ University Revisions-Notes/JSscript/show-lecture.js
@@ -0,0 +1,9 @@
+function show_lectures() {
+    const Mthd = document.getElementById("Mthd");
+    
+    if(Mthd.style.display === "none"){
+      Mthd.style.display = "grid";
+    }else{
+    Mthd.style.display = "none";
+    }
+    }
